42ea21eb9da92b2b68f1057dda6f29a55d055618
Shakib448 <shakiba448@gmail.com>
2020-12-25 16:19:23 +0600

    Album redux created

---
+++ src/redux/slice/albumSlice.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Album
+
+export const albumApiData = createAsyncThunk("album/albumContent", async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/albums"
+  );
+  return data;
+});
+
+const albumSlice = createSlice({
+  name: "image",
+  initialState: {
+    loading: true,
+    albumData: [],
+  },
+  extraReducers: {
+    [albumApiData.fulfilled]: (state, actions) => {
+      state.albumData = actions.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export default albumSlice.reducer;
+
+// selector
+
+export const dataImage = (state) => state.entities.image;
