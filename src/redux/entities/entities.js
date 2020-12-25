e7fd1fe6036f1b93a0c90917d11e06ba0c2a2527
Shakib448 <shakiba448@gmail.com>
2020-12-25 16:06:47 +0600

    Post redux method added

--- src/redux/entities/entities.js
+++ src/redux/entities/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import nasaReducers from "../slice/nasaSlice";
 import imageReducers from "../slice/imageSlice";
+import postReducer from "../slice/postSlice";
 
 export default combineReducers({
   nasa: nasaReducers,
   image: imageReducers,
+  post: postReducer,
 });
---
+++ src/redux/slice/postSlice.js
@@ -0,0 +1,58 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Post Data
+
+export const postData = createAsyncThunk("post/postContent", async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return data;
+});
+// Post fetch by  id
+export const postById = createAsyncThunk("post/postById", async (id) => {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  return data;
+});
+
+// Post fetch by  id comment
+export const postByIdComment = createAsyncThunk(
+  "post/postByIdComment",
+  async (id) => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/comments?postId=${id}`
+    );
+    return data;
+  }
+);
+
+const postSlice = createSlice({
+  name: "image",
+  initialState: {
+    loading: true,
+    postApiData: [],
+    postByIdData: [],
+  },
+  extraReducers: {
+    [postData.fulfilled]: (state, actions) => {
+      state.postApiData = actions.payload;
+      state.loading = false;
+    },
+    [postById.fulfilled]: (state, actions) => {
+      state.postByIdData = actions.payload;
+      state.loading = false;
+    },
+    [postByIdComment.fulfilled]: (state, actions) => {
+      state.postByIdData = actions.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export default postSlice.reducer;
+
+// selector
+
+export const dataPost = (state) => state.entities.post;
